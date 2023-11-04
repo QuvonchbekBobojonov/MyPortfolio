6e41d34310dc94d05f7772db64af57fe6c7e4b10
Quvonchbek Bobojonov <moorfoinfo@gmail.com>
2023-11-04 09:43:37 +0500

    instaled bootstrap

--- frontend/src/components/contact/index.jsx
+++ frontend/src/components/contact/index.jsx
@@ -32,19 +32,67 @@ function Contact() {
                 Xabaringiz muvaffaqiyatli yuborildi.
               </div>
             )}
-            <div className="row">
-              <div className="col-md-6">
-                {/* Other input fields */}
-              </div>
-              {/* Rest of the form input fields */}
-              <div className="col-md-12">
-                <div className="input-group submit-btn-wrap">
-                  <button className="theme-btn" name="submit" type="submit" id="submit-form">
-                    xabar yuborish
-                  </button>
-                </div>
-              </div>
-            </div>
+            <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="input-group">
+                                            <label for="full-name">to'liq ism <sup>*</sup></label>
+                                            <input type="text" name="full-name" id="full-name"
+                                                placeholder="To'liq ismingiz" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="input-group">
+                                            <label for="email">Email <sup>*</sup></label>
+                                            <input type="email" name="email" id="email"
+                                                placeholder="Sizning e-pochta manzilingiz" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="input-group">
+                                            <label for="phone-number">telefon <span>(ixtiyoriy)</span></label>
+                                            <input type="text" name="phone-number" id="phone-number"
+                                                placeholder="Telefon raqamingiz" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="input-group">
+                                            <label for="subject">Mavzu <sup>*</sup></label>
+                                            <input type="text" name="subject" id="subject" placeholder="Mavru kiriting" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="input-group">
+                                            <label for="budget">sizning byudjetingiz <span>(ixtiyoriy)</span></label>
+                                            <input type="number" name="budget" id="budget"
+                                                placeholder="Loyihangiz uchun bir qator byudjet" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="input-group">
+                                            <label for="message">xabar</label>
+                                            <textarea name="message" id="message"
+                                                placeholder="Xabaringizni shu yerga yozing..."></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="input-group upload-attachment">
+                                            <div>
+                                                <label for="upload-attachment">
+                                                    <i class="las la-cloud-upload-alt"></i> fayl yuklang
+                                                    <input type="file" name="file" id="upload-attachment" />
+                                                </label>
+
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="input-group submit-btn-wrap">
+                                            <button class="theme-btn" name="submit" type="submit" id="submit-form">xabar
+                                                yuborish</button>
+                                        </div>
+                                    </div>
+                                </div>
           </form>
         </div>
       </div>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-import './assets/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import './assets/css/owl.carousel.min.css'
 import './assets/css/owl.theme.default.min.css'
 import './assets/css/animate.min.css'
