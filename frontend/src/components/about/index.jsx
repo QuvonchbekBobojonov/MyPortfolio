b278d1aa2458e48b0f476988cb09801a19d44569
Quvonchbek Bobojonov <moorfoinfo@gmail.com>
2024-03-07 09:27:34 +0500

    add loader

--- frontend/src/components/about/index.jsx
+++ frontend/src/components/about/index.jsx
@@ -15,7 +15,7 @@ function About() {
                     <p className="scroll-animation" data-animation="fade_from_bottom">
                         Men Full stack dasturchisiman. Men yangi narsalarni yaratishni va murakkab muammolarni oddiy, chiroyli va intuitiv dizaynga aylantirishni yaxshi ko‘raman. Mening vazifam sizning veb-saytingiz va ilovalaringizni funksional va foydalanuvchilarga qulay, lekin ayni paytda jozibador bo‘lishi uchun yaratishdir. Bundan tashqari, men sizning mahsulotingizga shaxsiy teginish qo‘shaman va uning ko‘zni qamashtiruvchi va ishlatish uchun qulayligiga ishonch hosil qilaman. Mening maqsadim sizning xabaringiz va shaxsingizni ijodiy tarzda yetkazishdir.
                     </p>
-                    <div className="row mt-3">
+                    <div className="row mt-3 scroll-animation row-cols-1 row-cols-md-2" data-animation="fade_from_bottom">
                         <div className="col scroll-animation" data-animation="fade_from_left">
                             <ul>
                                 <li>Ism: Quvonchbek</li>
