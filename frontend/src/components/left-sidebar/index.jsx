ce113e6ca6e361e7b041abf5ff68c86c96bc5e9d
Quvonchbek Bobojonov <111079758+QuvonchbekBobojonov@users.noreply.github.com>
2025-01-15 21:41:34 +0500

    Update index.jsx

--- frontend/src/components/left-sidebar/index.jsx
+++ frontend/src/components/left-sidebar/index.jsx
@@ -17,16 +17,16 @@ function LeftSidebar() {
                     <a href="https://www.linkedin.com/in/moorfo/" target='_blank' rel="noreferrer"><i className="lab la-linkedin-in"></i></a>
                 </li>
                 <li>
-                    <a href="https://t.me/QuvonchbekBobojonov" target='_blank' rel="noreferrer"><i className="lab la-telegram"></i></a>
+                    <a href="https://t.me/+998774040066" target='_blank' rel="noreferrer"><i className="lab la-telegram"></i></a>
                 </li>
                 <li>
-                    <a href="https://instagram.com/moorfo.uz?igshid=NzZlODBkYWE4Ng==" target='_blank' rel="noreferrer"><i className="lab la-instagram"></i></a>
+                    <a href="https://instagram.com/quvonchbekdev" target='_blank' rel="noreferrer"><i className="lab la-instagram"></i></a>
                 </li>
                 <li>
                     <a href="https://github.com/QuvonchbekBobojonov" target='_blank' rel="noreferrer"><i className="lab la-github"></i></a>
                 </li>
             </ul>
-            <a href="https://quvonchbek_bobojonov.t.me/" target='_blank' className="theme-btn" rel="noreferrer">
+            <a href="tel:+998774040066" target='_blank' className="theme-btn" rel="noreferrer">
                 {/* eslint-disable-next-line react/no-unescaped-entities */}
                 <i className="las la-envelope"></i> Bog'lanish
             </a>
