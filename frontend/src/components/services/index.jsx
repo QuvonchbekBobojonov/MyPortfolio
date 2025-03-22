c8c625bb8d0b961dd38bf5cf9e5f865c609f80b5
Quvonchbek Bobojonov <111079758+QuvonchbekBobojonov@users.noreply.github.com>
2025-03-22 15:49:29 +0500

    Update index.jsx

--- frontend/src/components/services/index.jsx
+++ frontend/src/components/services/index.jsx
@@ -7,32 +7,31 @@ function Services() {
                 <div className="services-content content-width">
                     <div className="section-header">
                         <h4 className="subtitle scroll-animation" data-animation="fade_from_bottom">
-                            <i className="las la-stream"></i> Hizmatlar
+                            <i className="las la-stream"></i> Services
                         </h4>
                         <h1 className="scroll-animation" data-animation="fade_from_bottom">
-                            Sizga nimalarni <span> taklif qila olaman?</span>
+                            What can I <span>offer you?</span>
                         </h1>
                     </div>
 
                     <div className="services-items">
                         <div className="service-item scroll-animation" data-animation="fade_from_bottom">
                             <i className="las la-bezier-curve"></i>
-                            <h2>Veb sayt yaratish</h2>
-                            <p>Istalgan turdagi veb-saytlar tayyorlash. Masalan: Internet do‘kon, Blog, Axborot Beruvchi Saytlar!</p>
-                            <span className="projects">12+ loyihalar</span>
+                            <h2>Website Development</h2>
+                            <p>Creating websites of any type, such as online stores, blogs, and informational websites!</p>
+                            <span className="projects">12+ projects</span>
                         </div>
                         <div className="service-item scroll-animation" data-animation="fade_from_bottom">
                             <i className="las la-code"></i>
-                            <h2>Telegram bot</h2>
-                            <p>Istalgan Turdagi Telegram Bot Tayyorlash Va Uni Serverga Qo'yish. Sifatli
-                                Ko'rinishda</p>
-                            <span className="projects">10+ loyihalar</span>
+                            <h2>Telegram Bot Development</h2>
+                            <p>Developing Telegram bots of any kind and deploying them on a server with a professional appearance.</p>
+                            <span className="projects">10+ projects</span>
                         </div>
                         <div className="service-item scroll-animation" data-animation="fade_from_bottom">
                             <i className="las la-bezier-curve"></i>
-                            <h2>SEO optimallashtirish</h2>
-                            <p> Saytni Google, Yandex va boshqa qidiruv tizimlarda optimallashtirish bilan shug‘ullanamiz.</p>
-                            <span className="projects">5+ loyihalar</span>
+                            <h2>SEO Optimization</h2>
+                            <p>Improving website visibility in Google, Yandex, and other search engines.</p>
+                            <span className="projects">5+ projects</span>
                         </div>
                     </div>
 
