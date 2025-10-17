3a907eba9129abf4cedeedac6febedd7cc584aa0
Quvonchbek Bobojonov <hi@moorfo.uz>
2025-10-17 11:01:51 +0500

    Refactor skills section to use dynamic data mapping for improved maintainability

--- frontend/src/components/skills/index.jsx
+++ frontend/src/components/skills/index.jsx
@@ -10,6 +10,21 @@ import PostgresImage from '../../assets/postgresql.png';
 import SQLiteImage from '../../assets/sqlite.png';
 import RedisImage from '../../assets/redis.png';
 
+const skillsData = [
+    {name: "Python", image: pythonImage, projects: "15+"},
+    {name: "Django", image: DjangoImage, projects: "10+"},
+    {name: "Django REST Framework", image: DjangoRestImage, projects: "8+"},
+    {name: "Qt for Python (PySide6)", image: QTImage, projects: "2+"},
+    {name: "JavaScript", image: JavaScriptImage, projects: "12+"},
+    {name: "WordPress", image: WordPressImage, projects: "5+"},
+    {name: "Docker", image: DockerImage, projects: "5+"},
+    {name: "Nginx", image: NginxImage, projects: "5+"},
+    {name: "PostgreSQL", image: PostgresImage, projects: "10+"},
+    {name: "SQLite", image: SQLiteImage, projects: "10+"},
+    {name: "Redis", image: RedisImage, projects: "3+"},
+    {name: "MQTT (Broker & Client)", image: 'https://user-images.githubusercontent.com/20068373/149942025-f8282889-15e5-4f8e-a40f-dc1c2b0be4fd.png', projects: "2+"},
+];
+
 function Skills() {
     return (
         <section className="skills-area page-section scroll-to-page" id="skills">
@@ -25,128 +40,21 @@ function Skills() {
                     </div>
 
                     <div className="row skills text-center">
-
-                        {/* Python */}
-                        <div className="col-md-3 scroll-animation" data-animation="fade_from_bottom">
-                            <div className="skill">
-                                <div className="skill-inner">
-                                    <img src={pythonImage} alt="Python" width="70px" />
-                                    <h1 className="percent">15+<br/> projects</h1>
+                        {skillsData.map((skill, index) => (
+                            <div
+                                key={index}
+                                className="col-md-3 scroll-animation"
+                                data-animation="fade_from_bottom"
+                            >
+                                <div className="skill">
+                                    <div className="skill-inner">
+                                        <img src={skill.image} alt={skill.name} width="70px"/>
+                                        <h1 className="percent">{skill.projects}<br/> projects</h1>
+                                    </div>
+                                    <p className="name">{skill.name}</p>
                                 </div>
-                                <p className="name">Python</p>
                             </div>
-                        </div>
-
-                        {/* Django */}
-                        <div className="col-md-3 scroll-animation" data-animation="fade_from_bottom">
-                            <div className="skill">
-                                <div className="skill-inner">
-                                    <img src={DjangoImage} alt="Django" width="60px" />
-                                    <h1 className="percent">10+<br/> projects</h1>
-                                </div>
-                                <p className="name">Django</p>
-                            </div>
-                        </div>
-
-                        {/* Django REST Framework */}
-                        <div className="col-md-3 scroll-animation" data-animation="fade_from_bottom">
-                            <div className="skill">
-                                <div className="skill-inner">
-                                    <img src={DjangoRestImage} alt="Django REST Framework" width="70px" />
-                                    <h1 className="percent">8+<br/> projects</h1>
-                                </div>
-                                <p className="name">Django REST Framework</p>
-                            </div>
-                        </div>
-
-                        {/* Qt for Python */}
-                        <div className="col-md-3 scroll-animation" data-animation="fade_from_bottom">
-                            <div className="skill">
-                                <div className="skill-inner">
-                                    <img src={QTImage} alt="Qt for Python" width="70px" />
-                                    <h1 className="percent">2+<br/> projects</h1>
-                                </div>
-                                <p className="name">Qt for Python (PySide6)</p>
-                            </div>
-                        </div>
-
-                        {/* JavaScript */}
-                        <div className="col-md-3 scroll-animation" data-animation="fade_from_bottom">
-                            <div className="skill">
-                                <div className="skill-inner">
-                                    <img src={JavaScriptImage} alt="JavaScript" width="70px" />
-                                    <h1 className="percent">12+<br/> projects</h1>
-                                </div>
-                                <p className="name">JavaScript</p>
-                            </div>
-                        </div>
-
-                        {/* WordPress */}
-                        <div className="col-md-3 scroll-animation" data-animation="fade_from_bottom">
-                            <div className="skill">
-                                <div className="skill-inner">
-                                    <img src={WordPressImage} alt="WordPress" width="70px" />
-                                    <h1 className="percent">5+<br/> projects</h1>
-                                </div>
-                                <p className="name">WordPress</p>
-                            </div>
-                        </div>
-
-                        {/* Docker */}
-                        <div className="col-md-3 scroll-animation" data-animation="fade_from_bottom">
-                            <div className="skill">
-                                <div className="skill-inner">
-                                    <img src={DockerImage} alt="Docker" width="70px" />
-                                    <h1 className="percent">5+<br/> projects</h1>
-                                </div>
-                                <p className="name">Docker</p>
-                            </div>
-                        </div>
-
-                        {/* Nginx */}
-                        <div className="col-md-3 scroll-animation" data-animation="fade_from_bottom">
-                            <div className="skill">
-                                <div className="skill-inner">
-                                    <img src={NginxImage} alt="Nginx" width="70px" />
-                                    <h1 className="percent">5+<br/> projects</h1>
-                                </div>
-                                <p className="name">Nginx</p>
-                            </div>
-                        </div>
-
-                        {/* PostgreSQL */}
-                        <div className="col-md-3 scroll-animation" data-animation="fade_from_bottom">
-                            <div className="skill">
-                                <div className="skill-inner">
-                                    <img src={PostgresImage} alt="PostgreSQL" width="70px" />
-                                    <h1 className="percent">10+<br/> projects</h1>
-                                </div>
-                                <p className="name">PostgreSQL</p>
-                            </div>
-                        </div>
-
-                        {/* SQLite */}
-                        <div className="col-md-3 scroll-animation" data-animation="fade_from_bottom">
-                            <div className="skill">
-                                <div className="skill-inner">
-                                    <img src={SQLiteImage} alt="SQLite" width="70px" />
-                                    <h1 className="percent">10+<br/> projects</h1>
-                                </div>
-                                <p className="name">SQLite</p>
-                            </div>
-                        </div>
-
-                        {/* Redis */}
-                        <div className="col-md-3 scroll-animation" data-animation="fade_from_bottom">
-                            <div className="skill">
-                                <div className="skill-inner">
-                                    <img src={RedisImage} alt="Redis" width="70px" />
-                                    <h1 className="percent">3+<br/> projects</h1>
-                                </div>
-                                <p className="name">Redis</p>
-                            </div>
-                        </div>
-
+                        ))}
                     </div>
                 </div>
             </div>
